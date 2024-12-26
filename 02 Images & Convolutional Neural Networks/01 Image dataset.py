63452862b85c34d845d18ec1a44884d330241cd9
Sabin <spradh@users.noreply.github.com>
2024-12-26 12:56:50 -0600

    Update 01 Image dataset.py

--- 02 Images & Convolutional Neural Networks/01 Image dataset.py
+++ 02 Images & Convolutional Neural Networks/01 Image dataset.py
@@ -1,1 +1,12 @@
 
+# Compose transformations
+train_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize((128,128)),
+])
+
+# Create Dataset using ImageFolder
+dataset_train = ImageFolder(
+    "clouds_train",
+    transform=train_transforms,
+)
