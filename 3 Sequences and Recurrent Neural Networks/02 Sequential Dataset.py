2a8f07f8a2cb4a9b5051e2186c9a4a7f79b45274
Sabin <spradh@users.noreply.github.com>
2024-12-27 15:34:07 -0600

    Update 02 Sequential Dataset.py

--- 3 Sequences and Recurrent Neural Networks/02 Sequential Dataset.py
+++ 3 Sequences and Recurrent Neural Networks/02 Sequential Dataset.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import TensorDataset
 
 # Use create_sequences to create inputs and targets
-X_train, y_train = create_sequences(train_data)
+X_train, y_train = create_sequences(train_data, 24*4)
 print(X_train.shape, y_train.shape)
 
 # Create TensorDataset
