f44edd8ff0257fef58123813556553246abbee2d
Sabin <spradh@users.noreply.github.com>
2024-12-30 11:24:28 -0600

    Create 01 Two-input dataset.py

---
+++ 4 Multi=Input & Multi-Output Architectures/01 Two-input dataset.py
@@ -0,0 +1,17 @@
+class OmniglotDataset(Dataset):
+    def __init__(self, transform, samples):
+		# Assign transform and samples to class attributes
+        self.transform = transform
+        self.samples = samples
+                    
+    def __len__(self):
+		# Return number of samples
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+      	# Unpack the sample at index idx
+        img_path, alphabet, label = self.samples[idx]
+        img = Image.open(img_path).convert('L')
+        # Transform the image 
+        img_transformed = self.transform(img)
+        return img_transformed, alphabet, label
